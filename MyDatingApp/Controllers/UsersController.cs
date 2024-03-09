c90c1cd6d72c9becd99bf165a610ce89e77f5b89
nitindevgon <nitindevgon@aol.com>
2024-03-10 00:13:58 +1030

    First Commit

---
+++ MyDatingApp/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyDatingApp.Data;
+using MyDatingApp.Entities;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MyDatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UsersController(DataContext context) { 
+            _dataContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dataContext.appusers.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet ("{id}")]
+
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user =  await _dataContext.appusers.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound(); // Assuming you want to return a 404 Not Found if the user doesn't exist
+            }
+            return user;
+        }
+    }
+}
